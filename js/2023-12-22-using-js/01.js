15c80553fe656ed6d13c47e42a7dbe2e44489bd1
Burak Kuyucaklı <burak@Buraks-Mac-Studio.local>
2023-12-26 14:21:38 +0300

    Add js folders

---
+++ js/2023-12-22-using-js/01.js
@@ -0,0 +1,28 @@
+//single line code comment ( tek satır yorum )
+/*
+multi
+line
+code
+comment
+
+çoklu
+satır
+kod
+yorumu
+*/
+
+/* 
+
+output yöntemlerinde iki tanesi
+
+alert();
+
+console.log();
+
+
+
+*/
+
+// alert( "Hello World from the external js file" );
+
+console.log("Hello from the console logger. 1", "deneme", "deneme3");
