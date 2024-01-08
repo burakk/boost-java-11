9d044d087a6862d26bc6ad25ef60fe20086de46c
Burak Kuyucaklı <burak.kuyucakli@gmail.com>
2024-01-08 10:31:01 +0300

    Add react essentials part 2

---
+++ react/react-essentials-02/components/Logo.jsx
@@ -0,0 +1,11 @@
+import { IconBolt } from "./Icons"
+
+export function Logo(){
+    return(
+        <a href="#">
+            <IconBolt color="red"/>
+            Java-11
+        </a>
+    )
+
+}
---
+++ react/react-essentials-02/src/App.jsx
@@ -0,0 +1,14 @@
+import { Logo as MyLogo } from "../components/Logo"
+
+export default function MyApp() {
+
+  return (
+    <>
+      <header>
+        <MyLogo/>
+      </header>
+      <main>M</main>
+      <footer>F</footer>
+    </>
+  )
+}
